64cecfb179ccad9c5ccc233e7806643d6a6887b3
eddyangelop <eddysoullive@gmail.com>
2023-06-12 19:43:56 -0300

    foi criada as interfaces do projeto para receber os metodos do serviço da controller e os repositorios para comunicar com o banco de dados.

---
+++ ControleProcedure.DATA/Repsitories/RepositoryCliente.cs
@@ -0,0 +1,15 @@
+﻿using ControleProcedure.DATA.Interfaces;
+using ControleProcedure.DATA.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleProcedure.DATA.Repsitories
+{
+    public class RepositoryCliente : RepositoryBase<Cliente>, IRepositoryCliente
+    {
+        public RepositoryCliente(bool SaveChanges = true) : base(SaveChanges)
+        {
+        }
+    }
+}
---
+++ ControleProcedure.DATA/Repsitories/RepositoryClientesProcedimentos.cs
@@ -0,0 +1,15 @@
+﻿using ControleProcedure.DATA.Interfaces;
+using ControleProcedure.DATA.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleProcedure.DATA.Repsitories
+{
+    public class RepositoryClientesProcedimentos : RepositoryBase<ClientesProcedimentos>, IRepositoryClientesProcedimentos
+    {
+        public RepositoryClientesProcedimentos(bool SaveChanges = true) : base(SaveChanges)
+        {
+        }
+    }
+}
